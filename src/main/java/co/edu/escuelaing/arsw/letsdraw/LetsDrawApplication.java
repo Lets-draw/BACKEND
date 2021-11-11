d9400403d57f77a0b179f21a53e777b7fa194ecd
jua nsebastina garcia hincapie <juan.garcia-h@mail.escuelaing.edu.co>
2021-11-11 07:33:16 -0500

    main class

--- src/main/java/co/edu/escuelaing/arsw/letsdraw/LetsDrawApplication.java
+++ src/main/java/co/edu/escuelaing/arsw/letsdraw/LetsDrawApplication.java
@@ -7,20 +7,20 @@ package co.edu.escuelaing.arsw.letsdraw;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;import org.springframework.context.annotation.ComponentScan;
 
 /**
  *
  * @author jgarc
  */
 @SpringBootApplication
-@ComponentScan("co.edu.escuelaing.arsw.letsdraw")
-public class LetsDrawApplication implements CommandLineRunner {
+//@ComponentScan("co.edu.escuelaing.arsw.letsdraw")
+public class LetsDrawApplication {
     public static void main(String[] args) {
         SpringApplication.run(LetsDrawApplication.class, args);
     }
-    @Override
+    
+    //@Override
     public void run(String... args) throws Exception {
     }
 }
