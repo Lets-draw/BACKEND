41d4c366f2e89d3934831892bbdd932e77117843
jua nsebastina garcia hincapie <juan.garcia-h@mail.escuelaing.edu.co>
2021-11-11 07:25:37 -0500

    changes

--- src/main/java/co/edu/escuelaing/arsw/letsdraw/LetsDrawApplication.java
+++ src/main/java/co/edu/escuelaing/arsw/letsdraw/LetsDrawApplication.java
@@ -9,13 +9,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
+
 /**
  *
  * @author jgarc
  */
 @SpringBootApplication
-@EnableScheduling
 @ComponentScan("co.edu.escuelaing.arsw.letsdraw")
 public class LetsDrawApplication implements CommandLineRunner {
     public static void main(String[] args) {
